0f9dc9f325f9d8ad5e4bfdc3d671eeea549a40b8
Martin Lavoie <broemartino@gmail.com>
2016-04-19 17:28:20 -0400

    Update config

--- projects/cat_dog/config.py
+++ projects/cat_dog/config.py
@@ -1,11 +1,20 @@
 from itertools import product
 
-dropout = ["-d"]
-l1 = ["--L1"]
-l2 = ["--L2"]
+def optional(l):
+    return l+[""]
 
-std_parameters = ["--finish 1"]
-variable_parameters = [dropout, l1, l2]
+def lambda_generator(i):
+    return str([0.01, 0.05, 0.1][i])
+    from random import uniform
+    return str(uniform(0.01,0.2))
+
+dropout = optional(["-d"])
+l1 = optional(["--L1 " + lambda_generator(i) for i in range(1, 3)])
+l2 = optional(["--L2 " + lambda_generator(i) for i in range(1, 3)])
+update = optional(["-u rmsprop"])
+
+std_parameters = ["--finish 1", "-e"]
+variable_parameters = [dropout, l1, l2, update]
 
 def create_parameters_list(std, var):
     def extend(l):
@@ -13,13 +22,11 @@ def create_parameters_list(std, var):
             return l+e
         return extend_inside
 
-    var = map(extend([""]), var)
     var = map(list,product(*var))
 
     return list(map(extend(std), var))
 
 parameters_list = create_parameters_list(std_parameters, variable_parameters)
-
 
 if __name__=="__main__":
     print(len(parameters_list))
